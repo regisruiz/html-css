07a0cc5a04eeab8a07c6f82b96301795c818e6dd
Reginaldo Ruiz <regis.ruiz11@gmail.com>
2024-08-23 11:43:34 -0300

    criei form 003

--- exercicios/ex025/cadastro.php
+++ exercicios/ex025/cadastro.php
@@ -1,1 +1,17 @@
-Página PHP.
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+<?php 
+$nome = $_GET['cnome'];
+$sobrenome = $_GET['csobrenome'];
+print "Nome: <strong>$nome</strong><br/>";
+print "Sobrenome: <strong>$sobrenome</strong>";
+?>
+</body>
+</html>
+
